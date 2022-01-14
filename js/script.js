9b32ab54e060a694d45f5d224ed726fe33c70264
Salvo170586SP <pitanza.salvatore.86@gmail.com>
2022-01-14 20:48:57 +0100

    added functions input element

--- js/script.js
+++ js/script.js
@@ -6,14 +6,34 @@ Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 */
 
- 
+//recupero elementi in pagina
+const inputElement = document.getElementById('input-text');
+const buttonElement = document.getElementById('calculate');
+const resetElement = document.getElementById('reset');
+const resultElement = document.getElementById('result');
 
-const userWord = prompt('inserisci parola').trim();
 
-console.log(isPalidroma(userWord));
+//aggancio il bottone 'AVVIA' all'evento
+buttonElement.addEventListener('click', function(){
+    //creo variabile per definire il valore (stringa scritta) dell'input
+    const inputValue = inputElement.value;
+    console.log(inputValue);
+    //scrivo il risultato in paginas
+    resultElement.innerText = `LA PAROLA ${inputValue} ${isPalidroma(inputValue)}`;
+    console.log(isPalidroma(inputValue));
+})
+
+
+//aggancio il bottone 'CANCELLA' all'evento
+resetElement.addEventListener('click', function(){
+    inputElement.value = '';
+})
+
+
+
 
 //creo funzione 
-function isPalidroma(word){
+function isPalidroma(word) {
 
     let wordReverse = '';
     //creo ciclo per contare a ritroso le lettere tante quanto sono nella parola partendo dall'ultima
@@ -24,10 +44,13 @@ function isPalidroma(word){
     
     //condizione
     //se la parola è uguale alla parola inversa
-    if(word === wordReverse){
-        return true; //è vero
-    }else{ //altrimenti è falso
-        return false;
+    if (word === wordReverse) {
+        return  "E' PALIDROMA"; //è vero
+    } else { //altrimenti è falso
+        return "NON E' PALIDROMA";
     }
 }
-    
+
+
+
+
