eac73b90e9d48c0cb7af1e998fb718779a4898c8
Salvo170586SP <pitanza.salvatore.86@gmail.com>
2022-01-14 16:02:23 +0100

    added function

--- js/script.js
+++ js/script.js
@@ -23,18 +23,25 @@ Dichiariamo chi ha vinto.
 //chiedo all'utente inserire parola
 const userWord = prompt('inserisci una parola', 'ciao').trim();
 
-let wordReverse = '';
-//creo ciclo per contare a ritroso le lettere tante quanto sono nella parola partendo dall'ultima
-for (let i = userWord.length - 1; i >= 0; i--) {
-    //aggiungo tutte le lettere partendo dalla fine alla variabile wordReverse
-    wordReverse += userWord[i];
+console.log(isPalidroma(userWord));
+
+
+//creo funzione 
+function isPalidroma(word){
+
+    let wordReverse = '';
+    //creo ciclo per contare a ritroso le lettere tante quanto sono nella parola partendo dall'ultima
+    for (let i = word.length - 1; i >= 0; i--) {
+        //aggiungo tutte le lettere partendo dalla fine alla variabile wordReverse
+        wordReverse += word[i];
+    }
+    
+    //condizione
+    //se la parola è uguale alla parola inversa
+    if(word === wordReverse){
+        return true; //è vero
+    }else{ //altrimenti è falso
+        return false;
+    }
 }
-
-console.log(wordReverse);
-
-let isValid = false;
-
-if(userWord === wordReverse){
-    isValid = true;
-}
-console.log(isValid);
+    
